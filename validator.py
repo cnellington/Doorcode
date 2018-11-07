95125b1e94be3eb5b66b56942da995dfbfbd582d
cnellington <caleb.n.ellington@gmail.com>
2018-11-07 10:07:53 -0800

    swipe card reader implemented

---
+++ validator.py
@@ -0,0 +1,20 @@
+# ;10016370535670?
+
+class student:
+	name = "default"
+	st_id = -1
+	house = "non-greek"
+	year = -1
+
+	def __init__(self, name_in, st_id_in, house_in, year_in):
+		name = name_in
+		st_id = st_id_in
+		house = house_in
+		year = year_in
+
+	
+		
+val = input()
+val = val[4:11]
+print(val)
+caleb = student("caleb", val, "Phi Delt", 3)
