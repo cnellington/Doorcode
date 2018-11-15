0e3449d84bbc4f293f2de7c7f5898ca248e98548
cnellington <caleb.n.ellington@gmail.com>
2018-11-15 14:54:37 -0800

    add graphics and name readout

--- validator.py
+++ validator.py
@@ -1,5 +1,7 @@
-# ;10016370535670?
+from graphics import *
 
+# ;10016370535670?
+# caleb = student("caleb", val, "Phi Delt", 3)
 class student:
 	name = "default"
 	st_id = -1
@@ -12,9 +14,24 @@ class student:
 		house = house_in
 		year = year_in
 
-	
-		
-val = input()
-val = val[4:11]
-print(val)
-caleb = student("caleb", val, "Phi Delt", 3)
+whitelist = {'1637053': 'Joe Schmoe, Phi Delts'}
+
+def parseCode(code):
+	return code[4:11]
+
+def main():
+	win = GraphWin("DOORCODE", 800, 800)
+	win.setBackground("White")
+	while(True):
+		code = input()
+		doorcode = parseCode(code)
+		if(doorcode in whitelist.keys()):
+			win.setBackground("Green")
+			print(whitelist[doorcode])
+		else:
+			win.setBackground("Red")
+		time.sleep(.5)
+		win.setBackground("White")
+
+
+main()
